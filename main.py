174ec8390d87df6855f89f4800541bebdb624bfe
Martin-Ruggeri-Bio <martinruggeri18@gamil.com>
2022-09-26 20:46:39 -0300

    red neuronal con back propagation completo realizado 10000

--- main.py
+++ main.py
@@ -1,16 +1,75 @@
 from neurona_simple import NeuronaSimple
 
 if __name__ == '__main__':
-    red_neuronal = {}
+    print("xor 0, 0, 0")
     neurona_1= NeuronaSimple(entrada_1=0, entrada_2=0, peso_0=0.9, peso_1=0.7, peso_2=0.5)
     neurona_2 = NeuronaSimple(entrada_1=0, entrada_2=0, peso_0=0.3, peso_1=-0.9, peso_2=-1)
     neurona_3 = NeuronaSimple(entrada_1=0, entrada_2=0, peso_0=0.8, peso_1=0.35, peso_2=0.1)
     salida_1 = neurona_1.entrenamiento()
     salida_2 = neurona_2.entrenamiento()
     salida_3 = neurona_3.entrenamiento()
-    neurona_4 = NeuronaSimple(entrada_1=salida_1, entrada_2=salida_2, entrada_3=salida_3, peso_0=-0.23, peso_1=-0.79, peso_2=0.56, peso_3=0.6)
+    neurona_4 = NeuronaSimple(entrada_1=salida_1, entrada_2=salida_2, entrada_3=salida_3, peso_0=-0.23, peso_1=-0.79, peso_2=0.56, peso_3=0.6, salida_esperada=0)
     salida_4 = neurona_4.entrenamiento()
-    print(f"neurona_1: {neurona_1}")
-    print(f"neurona_2: {neurona_2}")
-    print(f"neurona_3: {neurona_3}")
+    for i in range(100000):
+        salida_1_back = neurona_1.entrenamiento_back(salida_4)
+        salida_2_back = neurona_2.entrenamiento_back(salida_4)
+        salida_3_back = neurona_3.entrenamiento_back(salida_4)
+        neurona_4.entrada_1 = salida_1_back
+        neurona_4.entrada_2 = salida_1_back
+        neurona_4.entrada_3 = salida_1_back
+        salida_4 = neurona_4.entrenamiento()
+    print(f"neurona_4: {neurona_4}")
+    print("xor 0, 1, 1")
+    neurona_1= NeuronaSimple(entrada_1=0, entrada_2=1, peso_0=0.9, peso_1=0.7, peso_2=0.5)
+    neurona_2 = NeuronaSimple(entrada_1=0, entrada_2=1, peso_0=0.3, peso_1=-0.9, peso_2=-1)
+    neurona_3 = NeuronaSimple(entrada_1=0, entrada_2=1, peso_0=0.8, peso_1=0.35, peso_2=0.1)
+    salida_1 = neurona_1.entrenamiento()
+    salida_2 = neurona_2.entrenamiento()
+    salida_3 = neurona_3.entrenamiento()
+    neurona_4 = NeuronaSimple(entrada_1=salida_1, entrada_2=salida_2, entrada_3=salida_3, peso_0=-0.23, peso_1=-0.79, peso_2=0.56, peso_3=0.6, salida_esperada=1)
+    salida_4 = neurona_4.entrenamiento()
+    for i in range(100000):
+        salida_1_back = neurona_1.entrenamiento_back(salida_4)
+        salida_2_back = neurona_2.entrenamiento_back(salida_4)
+        salida_3_back = neurona_3.entrenamiento_back(salida_4)
+        neurona_4.entrada_1 = salida_1_back
+        neurona_4.entrada_2 = salida_1_back
+        neurona_4.entrada_3 = salida_1_back
+        salida_4 = neurona_4.entrenamiento()
+    print(f"neurona_4: {neurona_4}")
+    print("xor 1, 0, 1")
+    neurona_1= NeuronaSimple(entrada_1=1, entrada_2=0, peso_0=0.9, peso_1=0.7, peso_2=0.5)
+    neurona_2 = NeuronaSimple(entrada_1=1, entrada_2=0, peso_0=0.3, peso_1=-0.9, peso_2=-1)
+    neurona_3 = NeuronaSimple(entrada_1=1, entrada_2=0, peso_0=0.8, peso_1=0.35, peso_2=0.1)
+    salida_1 = neurona_1.entrenamiento()
+    salida_2 = neurona_2.entrenamiento()
+    salida_3 = neurona_3.entrenamiento()
+    neurona_4 = NeuronaSimple(entrada_1=salida_1, entrada_2=salida_2, entrada_3=salida_3, peso_0=-0.23, peso_1=-0.79, peso_2=0.56, peso_3=0.6, salida_esperada=1)
+    salida_4 = neurona_4.entrenamiento()
+    for i in range(100000):
+        salida_1_back = neurona_1.entrenamiento_back(salida_4)
+        salida_2_back = neurona_2.entrenamiento_back(salida_4)
+        salida_3_back = neurona_3.entrenamiento_back(salida_4)
+        neurona_4.entrada_1 = salida_1_back
+        neurona_4.entrada_2 = salida_1_back
+        neurona_4.entrada_3 = salida_1_back
+        salida_4 = neurona_4.entrenamiento()
+    print(f"neurona_4: {neurona_4}")
+    print("xor 1, 1, 0")
+    neurona_1= NeuronaSimple(entrada_1=1, entrada_2=1, peso_0=0.9, peso_1=0.7, peso_2=0.5)
+    neurona_2 = NeuronaSimple(entrada_1=1, entrada_2=1, peso_0=0.3, peso_1=-0.9, peso_2=-1)
+    neurona_3 = NeuronaSimple(entrada_1=1, entrada_2=1, peso_0=0.8, peso_1=0.35, peso_2=0.1)
+    salida_1 = neurona_1.entrenamiento()
+    salida_2 = neurona_2.entrenamiento()
+    salida_3 = neurona_3.entrenamiento()
+    neurona_4 = NeuronaSimple(entrada_1=salida_1, entrada_2=salida_2, entrada_3=salida_3, peso_0=-0.23, peso_1=-0.79, peso_2=0.56, peso_3=0.6, salida_esperada=0)
+    salida_4 = neurona_4.entrenamiento()
+    for i in range(100000):
+        salida_1_back = neurona_1.entrenamiento_back(salida_4)
+        salida_2_back = neurona_2.entrenamiento_back(salida_4)
+        salida_3_back = neurona_3.entrenamiento_back(salida_4)
+        neurona_4.entrada_1 = salida_1_back
+        neurona_4.entrada_2 = salida_1_back
+        neurona_4.entrada_3 = salida_1_back
+        salida_4 = neurona_4.entrenamiento()
     print(f"neurona_4: {neurona_4}")
--- neurona_simple.py
+++ neurona_simple.py
@@ -1,7 +1,7 @@
 import math
 
 class NeuronaSimple():
-    def __init__(self, entrada_1, entrada_2, peso_0, peso_1, peso_2, peso_3=None, entrada_3=None,):
+    def __init__(self, entrada_1, entrada_2, peso_0=None, peso_1=None, peso_2=None, peso_3=None, entrada_3=None, salida_esperada=None):
         self.entrada_0 = 1
         self.entrada_1 = entrada_1
         self.entrada_2 = entrada_2
@@ -10,19 +10,45 @@ class NeuronaSimple():
         self.peso_1 = peso_1
         self.peso_2 = peso_2
         self.peso_3 = peso_3
+        self.salida_esperada = salida_esperada
         self.salida_real = 1
+        self.delta = 0
         self.x = 0
     
     def entrenamiento(self):
-        if self.entrada_3 and self.peso_3:
+        if self.entrada_3:
             x = (self.entrada_0 * self.peso_0) + (self.entrada_1 * self.peso_1) + (self.entrada_2 * self.peso_2) + (self.entrada_3 * self.peso_3)
             self.x = x
             self.salida_real = 1/(1+math.exp(x*(-1)))
+            variacion_abs = self.salida_esperada - self.salida_real
+            self.delta = self.salida_real*(1-self.salida_real)*variacion_abs
+            variacion_peso_0 = 0.1*self.entrada_0*self.delta
+            variacion_peso_1 = 0.1*self.entrada_1*self.delta
+            variacion_peso_2 = 0.1*self.entrada_2*self.delta
+            variacion_peso_3 = 0.1*self.entrada_3*self.delta
+            self.peso_0 = self.peso_0 + variacion_peso_0
+            self.peso_1 = self.peso_1 + variacion_peso_1
+            self.peso_2 = self.peso_2 + variacion_peso_2
+            self.peso_3 = self.peso_3 + variacion_peso_3
+            return self.delta
         else:
             x = (self.entrada_0 * self.peso_0) + (self.entrada_1 * self.peso_1) + (self.entrada_2 * self.peso_2)
             self.x = x
             self.salida_real = 1/(1+math.exp(x*(-1)))
-        return 1/(1+math.exp(x*(-1)))
+        return self.salida_real
+
+    def entrenamiento_back(self, delta):
+        delta_oculto = self.salida_real*(1-self.salida_real)*delta
+        variacion_peso_0 = 0.1*self.entrada_0*delta_oculto
+        variacion_peso_1 = 0.1*self.entrada_1*delta_oculto
+        variacion_peso_2 = 0.1*self.entrada_2*delta_oculto
+        self.peso_0 = self.peso_0 + variacion_peso_0
+        self.peso_1 = self.peso_1 + variacion_peso_1
+        self.peso_2 = self.peso_2 + variacion_peso_2
+        x = (self.entrada_0 * self.peso_0) + (self.entrada_1 * self.peso_1) + (self.entrada_2 * self.peso_2)
+        self.x = x
+        self.salida_real = 1/(1+math.exp(x*(-1)))
+        return self.salida_real
 
     def __str__(self):
         return (f"""
@@ -35,5 +61,7 @@ class NeuronaSimple():
             peso_2: {self.peso_2}
             peso_3: {self.peso_3}
             x: {self.x}
+            salida_esperada: {self.salida_esperada}
+            delta: {self.delta}
             salida_real: {self.salida_real}
         """)
