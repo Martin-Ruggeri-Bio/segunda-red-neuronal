684d30890cf7d4610bff1566b158fd26489c5f5c
Martin-Ruggeri-Bio <martinruggeri18@gamil.com>
2022-09-26 19:16:04 -0300

    errores arreglados

--- neurona_simple.py
+++ neurona_simple.py
@@ -11,12 +11,17 @@ class NeuronaSimple():
         self.peso_2 = peso_2
         self.peso_3 = peso_3
         self.salida_real = 1
+        self.x = 0
     
     def entrenamiento(self):
-        if self.entrada_3:
+        if self.entrada_3 and self.peso_3:
             x = (self.entrada_0 * self.peso_0) + (self.entrada_1 * self.peso_1) + (self.entrada_2 * self.peso_2) + (self.entrada_3 * self.peso_3)
-        x = (self.entrada_0 * self.peso_0) + (self.entrada_1 * self.peso_1) + (self.entrada_2 * self.peso_2)
-        self.salida_real = 1/(1+math.exp(x*(-1)))
+            self.x = x
+            self.salida_real = 1/(1+math.exp(x*(-1)))
+        else:
+            x = (self.entrada_0 * self.peso_0) + (self.entrada_1 * self.peso_1) + (self.entrada_2 * self.peso_2)
+            self.x = x
+            self.salida_real = 1/(1+math.exp(x*(-1)))
         return 1/(1+math.exp(x*(-1)))
 
     def __str__(self):
@@ -24,8 +29,11 @@ class NeuronaSimple():
             entrada 0: {self.entrada_0}
             entrada_1: {self.entrada_1}
             entrada_2: {self.entrada_2}
+            entrada_3: {self.entrada_3}
             peso_0: {self.peso_0}
             peso_1: {self.peso_1}
             peso_2: {self.peso_2}
+            peso_3: {self.peso_3}
+            x: {self.x}
             salida_real: {self.salida_real}
         """)
